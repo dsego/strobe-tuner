88aed5b6826c2aefa77f894d7a48360ee1d9bf5c
Davorin Šego <davorin.sego@gmail.com>
2024-10-28 22:57:08 +0100

    New swift xcode project

---
+++ swift-app/StrobeTuner/StrobeTunerTests/StrobeTunerTests.swift
@@ -0,0 +1,16 @@
+//
+//  StrobeTunerTests.swift
+//  StrobeTunerTests
+//
+//  Created by Davorin on 28.10.2024..
+//
+
+import Testing
+
+struct StrobeTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
